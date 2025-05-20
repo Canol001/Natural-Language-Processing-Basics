e375872b5b41c6e54c1d46fd17c02c893f28e490
O CANOL <canolowana6@gmail.com>
2025-05-21 00:35:55 +0300

    this version contains all files

--- nlp_web_app/app.py
+++ nlp_web_app/app.py
@@ -4,5 +4,9 @@ from app.routes import main
 app = Flask(__name__)
 app.register_blueprint(main)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=10000)
+
