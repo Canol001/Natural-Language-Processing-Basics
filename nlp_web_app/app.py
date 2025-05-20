e1a126a7f17b261140eb59d3756f6159bd5b90e7
O CANOL <canolowana6@gmail.com>
2025-05-21 01:32:58 +0300

    this version contains all files

--- nlp_web_app/app.py
+++ nlp_web_app/app.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from core.routes import main
 
-os.makedirs('static/audio', exist_ok=True)
-os.makedirs('static/pdfs', exist_ok=True)
-
 app = Flask(__name__)
 app.register_blueprint(main)
